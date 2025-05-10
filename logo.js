7c3dbddd633da6d3a1c722d24ad0b16d78ac4317
Felarwont <93824045+Felarwont@users.noreply.github.com>
2025-05-10 19:00:19 +0500

    Update logo.js

--- logo.js
+++ logo.js
@@ -48,6 +48,7 @@
 
           this.style.objectFit = 'contain';
           this.style.marginTop = '20px';
+          this.style.marginDown = '20px';
 
           e.object.activity.render()
             .find('.full-start-new__title')
